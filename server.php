7557e209a3846f72541b08cbd006bccb864531ee
luqman harun <luqmanharunsusanto@gmail.com>
2026-03-19 01:34:16 +0700

    Fix formatting in server.php by removing unnecessary spaces in file paths

--- server.php
+++ server.php
@@ -1,10 +1,11 @@
 <?php
+
 /*
 Router untuk PHP built-in server
 Menangani static files dan route dynamic requests ke index.php
 */
 
-$requested_file = __DIR__ . '/public' . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$requested_file = __DIR__.'/public'.parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 // Jika file static ada, serve langsung
 if (file_exists($requested_file)) {
@@ -12,4 +13,4 @@ if (file_exists($requested_file)) {
 }
 
 // Jika tidak ada, route ke index.php
-require __DIR__ . '/public/index.php';
+require __DIR__.'/public/index.php';
